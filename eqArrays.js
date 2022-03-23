fb3bf3f18a48631cecfd43e42ea9089c9578a18d
VincentHChoy <vhchoy@ualberta.ca>
2022-03-23 18:32:20 +0000

    created eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  // comparing the two inputs and assiging result the outcome
+  let result = actual === expected ? `😎😎😎 Assertion Passed: ${actual} === ${expected}` : `🤔🤔🤔 Assertion Failed: ${actual} !== ${expected}`;
+  return result;
+};
+
+const eqArrays = function(array1, array2) {
+  let i = 0;
+  let same = true;
+  while (same && i < array1.length) {
+    // console.log(assertEqual(array1[i], array2[i]))
+    same = array1[i] === array2[i] ? same : false;
+    i++;
+  }
+  return same;
+};
+
+
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
