8a3727ee9466524b43cf6a46546a80705b0ed805
VincentHChoy <vhchoy@ualberta.ca>
2022-03-28 17:09:47 +0000

    adding map.js

---
+++ map.js
@@ -0,0 +1,19 @@
+const assertArraysEqual = require('./assertArraysEqual.js');
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    // console.log('item BEFORE: ', item);
+    // console.log('item AFTER: ', callback(item));
+    // console.log('---');
+    results.push(callback(item));
+  }
+  return results;
+};
+
+//test cases
+// assertArraysEqual(map(words,word => word[0]),['g','c','t','m','t'])
+// assertArraysEqual(map(words,word => word.toUpperCase()),['GROUND','CONTROL','TO','MAJOR','TOM'])
+// assertArraysEqual(map(words,word => word + ' dog'),["ground dog", "control dog", "to dog", "major dog", "tom dog"])
