60dc6a1e3e794bba7b576f5fbc5f3f7d86139386
VincentHChoy <vhchoy@ualberta.ca>
2022-04-04 18:12:02 +0000

    changed flatten to be recursive

--- flatten.js
+++ flatten.js
@@ -1,14 +1,20 @@
-const flatten = function(array) {
-
-  return array.join().split(',').map(x => {
-
-    if (x == Number(x)) { // using double equals on purpose to get the loosely comparasion
-      return Number(x);
+const flatten = function(array, output = []) {
+  for (let i = 0; i < array.length; i++) { //base case is when it stops looping
+    if(Array.isArray(array[i])){
+      flatten(array[i],output)// recursive case, uses the output in the first function as the new output for when it enters array
     }
-    return x;
-  }
-  );
+    else{
+      output.push(array[i])
+
+    }
+  } 
+
+  return output
 };
+
+console.log(flatten([1, 2, [3, '4'],'dog', [6]])); // => [1, 2, 3, '4', 'dog', 6]
+
+// console.log([1,2].concat([3,'4']))
 
 module.exports = flatten;
 
